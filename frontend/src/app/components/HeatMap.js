92c14c39d68b300b0bb2103dbb2b12b3b6997414
jh0922 <vip0922@gmail.com>
2023-06-29 11:40:15 +0100

    -

--- frontend/src/app/components/HeatMap.js
+++ frontend/src/app/components/HeatMap.js
@@ -19,12 +19,21 @@ const Heatmap = () => {
 
         const cfg = {
             // radius: 2
+            // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+            // if scaleRadius is false it will be the constant radius used in pixels
             radius: 20,
             maxOpacity: 0.8,
+            // scales the radius based on map zoom
             scaleRadius: true,
+            // if set to false the heatmap uses the global maximum for colorization
+             // if activated: uses the data maximum within the current map boundaries
+            //   (there will always be a red spot with useLocalExtremas true)
             useLocalExtrema: true,
+            // which field name in your data represents the latitude - default "lat"
             latField: "lat",
+            // which field name in your data represents the longitude - default "lng"
             lngField: "lng",
+            // which field name in your data represents the data value - default "value"
             valueField: "count",
         };
 
