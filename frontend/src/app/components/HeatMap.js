e0a0a5ce386ad96d5337a7e4f49d3923ef213955
jh0922 <vip0922@gmail.com>
2023-06-29 01:32:46 +0100

    0629 update 2

--- frontend/src/app/components/HeatMap.js
+++ frontend/src/app/components/HeatMap.js
@@ -1,49 +1,40 @@
-import React, { useEffect } from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import React, {useEffect} from "react";
+import {useMap} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet.heat";
 
 const Heatmap = () => {
-  const map = useMap();
+    const map = useMap();
 
-  useEffect(() => {
-    const testData = {
-      max: 8,
-      data: [
-        { lat: 40.7283, lng: -73.9942, count: 30 },
-        { lat: -73.9942, lng: 40.7283, count: 10 },
+    useEffect(() => {
+        const testData = {
+            max: 8,
+            data: [
+                {lat: 40.7283, lng: -73.9942, count: 30},
+                {lat: 40.7583, lng: -73.9942, count: 10},
+                {lat: 40.7283, lng: -73.9842, count: 30},
 
-      ],
-    };
+            ],
+        };
 
-    const cfg = {
-      radius: 20,
-      maxOpacity: 0.8,
-      scaleRadius: true,
-      useLocalExtrema: true,
-      latField: "lat",
-      lngField: "lng",
-      valueField: "count",
-    };
+        const cfg = {
+            // radius: 2
+            radius: 20,
+            maxOpacity: 0.8,
+            scaleRadius: true,
+            useLocalExtrema: true,
+            latField: "lat",
+            lngField: "lng",
+            valueField: "count",
+        };
 
-    const heatmapLayer = L.heatLayer(
-      testData.data.map((item) => [item.lat, item.lng, item.count]),
-      cfg
-    ).addTo(map);
-  }, [map]);
+        const heatmapLayer = L.heatLayer(
+            testData.data.map((item) => [item.lat, item.lng, item.count]),
+            cfg
+        ).addTo(map);
+    }, [map]);
 
-  return null;
+    return null;
 };
-
-// const HeatmapMap2 = () => (
-//   <MapContainer center={[25.6586, -80.3568]} zoom={4}>
-//     <TileLayer
-//       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//       attribution="..."
-//       maxZoom={18}
-//     />
-//     <Heatmap />
-//   </MapContainer>
-// );
 
 export default Heatmap;
