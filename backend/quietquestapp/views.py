d639c8d020cbf01141a90590988ebe69e758f95b
MichaelMoyles <93867352+MichaelMoyles@users.noreply.github.com>
2023-06-30 13:18:32 +0100

    Optimised views (#22)
    * optimised views and updated tests
    
    * moved create buffer polygon function

--- backend/quietquestapp/views.py
+++ backend/quietquestapp/views.py
@@ -14,55 +14,23 @@ from datetime import datetime
 import pickle
 import pandas as pd
 import numpy as np
+import time
 
 
 # Expects POST operation from react front end, request contains the coordinates of the start and destination
 # will be expanded to include date and time
 @api_view(['POST'])
 def directions_view(request):
-    # Creates a polygon around the inputted point, used to find if a route passes through a point
-    def create_buffer_polygon(point_in, resolution=2, radius=20):
-        transformer_wgs84_to_utm32n = Transformer.from_crs("EPSG:4326", "EPSG:3857")
-        transformer_utm32n_to_wgs84 = Transformer.from_crs("EPSG:3857", "EPSG:4326")
+    start = time.time()
+
+    def create_buffer_polygon(transformer_wgs84_to_utm32n, transformer_utm32n_to_wgs84, point_in, resolution=2,
+                              radius=20):
         point_in_proj = transformer_wgs84_to_utm32n.transform(*point_in)
         point_buffer_proj = Point(point_in_proj).buffer(radius, resolution=resolution)  # 20 m buffer
 
-        # Iterate over all points in buffer and build polygon
-        poly_wgs = []
-        for point in point_buffer_proj.exterior.coords:
-            poly_wgs.append(transformer_utm32n_to_wgs84.transform(*point))  # Transform back to WGS84
+        # Transform back to WGS84
+        poly_wgs = [transformer_utm32n_to_wgs84.transform(*point) for point in point_buffer_proj.exterior.coords]
         return poly_wgs
-
-    # Loads in the api key
-    api_key = info.ors_key
-    # Sends request to open route service
-    ors = client.Client(key=api_key)
-    # Start and Destination of route from POST
-    coordinates = request.data
-
-    # Points that have a high index value
-    high_index_value_ls = []
-    # List of polygons for each point
-    point_geometry = []
-
-    # this temporarily uses the current time and date, will be updated for predictions later on by passing through
-    # the data and time parameters
-    all_locations = predicted_locations()
-
-    for location in all_locations:
-        # locations that have a count less than 4 are not 'noisy'/'busy'
-        if location['count'] >= 4:
-            position = [location['long'], location['lat']]
-            # Create buffer polygons around affected sites with 20 m radius and low resolution
-            point_buffer = create_buffer_polygon(position,
-                                                 resolution=2,  # low resolution to keep polygons lean
-                                                 radius=20)
-            high_index_value_ls.append(point_buffer)
-
-            # Create simplify geometry and merge overlapping buffer regions
-            point_poly = Polygon(point_buffer)
-            point_geometry.append(point_poly)
-    #union_poly = mapping(cascaded_union(point_geometry))
 
     # sends route request to api with start and destination coordinates and polygons to avoid
     def create_route(data, avoided_point_list, n=0):
@@ -86,7 +54,24 @@ def directions_view(request):
         dilated_route = new_linestring.buffer(0.001)
         return dilated_route
 
-    # Create empty list with avoided points
+    api_key = info.ors_key
+    ors = client.Client(key=api_key)
+    coordinates = request.data["locations"]
+
+    high_index_value_ls = []
+    point_geometry = []
+    transformer_wgs84_to_utm32n = Transformer.from_crs("EPSG:4326", "EPSG:3857")
+    transformer_utm32n_to_wgs84 = Transformer.from_crs("EPSG:3857", "EPSG:4326")
+
+    all_locations = predicted_locations()
+
+    for location in all_locations:
+        if location['count'] >= 4:
+            position = [location['long'], location['lat']]
+            point_buffer = create_buffer_polygon(transformer_wgs84_to_utm32n, transformer_utm32n_to_wgs84, position)
+            high_index_value_ls.append(point_buffer)
+            point_geometry.append(Polygon(point_buffer))
+
     avoided_point_list = []
     # Create regular route with still empty avoided_point_list
     optimal_directions = create_route(coordinates, avoided_point_list)
@@ -99,17 +84,16 @@ def directions_view(request):
             poly = Polygon(site_poly)
             if poly.within(avoidance_directions):
                 avoided_point_list.append(poly)
-
-                # Create new route and buffer
                 avoidance_route = create_route(coordinates, avoided_point_list, 1)
                 avoidance_directions = create_buffer(avoidance_route)
                 print('Generated alternative route, which avoids affected areas.')
-            else:
-                avoidance_route = ""
 
-    except Exception:
-        print(
-            'Sorry, there is no route available between the requested destination because of too many blocked streets.')
+    except Exception as e:
+        avoidance_route = ""
+        print(e)
+
+    end = time.time()
+    print("Elapsed time: " + str(end - start))
 
     # return json back to react front end
     return JsonResponse({
@@ -123,7 +107,7 @@ def directions_view(request):
 @api_view(['GET'])
 def locations_view(request):
     # gets the value of all long and lat objects in the database
-    location_data = Locations.objects.values('long', 'lat')[:100]
+    location_data = Locations.objects.values('long', 'lat')
 
     # gets the current time by hour
     now = datetime.now()
@@ -173,7 +157,7 @@ def locations_view(request):
 # such as date and time which will be used as inputs into the model
 def predicted_locations():
     # gets the value of all long and lat objects in the database
-    location_data = Locations.objects.values('long', 'lat')[:100]
+    location_data = Locations.objects.values('long', 'lat')
 
     # gets the current time by hour
     now = datetime.now()
@@ -218,4 +202,3 @@ def predicted_locations():
         response_data.append(data)
 
     return response_data
-
