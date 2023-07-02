852c2572dca0aa0565fbe64ada9a2a5495be43f0
jh0922 <vip0922@gmail.com>
2023-07-03 00:30:20 +0100

    update

--- frontend/src/app/components/tileLayer.js
+++ frontend/src/app/components/tileLayer.js
@@ -1,6 +1,7 @@
 const tileLayer = {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-  url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+  // url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+  url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
 }
 
 export default tileLayer;
