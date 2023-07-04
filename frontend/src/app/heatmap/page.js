1e4c20e9c05c73ce858c4eed26c40b7c15cfe3f0
jh0922 <vip0922@gmail.com>
2023-07-05 00:46:33 +0100

    routing test

--- frontend/src/app/heatmap/page.js
+++ frontend/src/app/heatmap/page.js
@@ -1,17 +1,16 @@
 "use client";
 import React from "react";
 import dynamic from "next/dynamic";
-import PostTest from "@/app/components/PostTest";
+import Routing2 from "@/app/components/Routing2";
 
-// const MapWithNoSSR = dynamic(() => import('../components/PostTest'), {
-//   ssr: false,
-// });
+const MapWithNoSSR = dynamic(() => import('../components/Routing2'), {
+  ssr: false,
+});
 
 export default function App() {
   return (
     <div className="App">
-      <PostTest />
-
+      <Routing2 />
       <style jsx>{`
         .App {
           display: flex;
