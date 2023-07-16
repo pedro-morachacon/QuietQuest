1ca2549d08bd4393fdb4ea5e45e42b0fd0bf06a7
jh0922 <vip0922@gmail.com>
2023-07-16 15:23:14 +0100

    -

---
+++ frontend/src/app/firebase/page.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+import Signup from "@/app/firebase/Signup";
+
+export default function App() {
+  return (
+      <div>
+        <Signup />
+      </div>
+  )
+};
+
