b96ea03a78a330a58ff0224541c280f36b89cdf0
jh0922 <vip0922@gmail.com>
2023-07-17 02:29:00 +0100

    firebase update

--- frontend/src/app/firbase.js
+++ frontend/src/app/firbase.js
@@ -17,20 +17,23 @@
 // export default app;
 
 
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAulItw5ca6s2MjQLJFxK30Mf8S4loNTD4",
+  authDomain: "react-auth-2d615.firebaseapp.com",
+  projectId: "react-auth-2d615",
+  storageBucket: "react-auth-2d615.appspot.com",
+  messagingSenderId: "1079924863455",
+  appId: "1:1079924863455:web:bb41cdb0a6831e51d5946a"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { auth };
-export default app;
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
--- frontend/src/app/firebase/page.js
+++ frontend/src/app/firebase/page.js
@@ -1,21 +1,40 @@
 "use client";
 import React from "react";
 
-import Signup from "@/app/firebase/Signup";
-import { Container } from "react-bootstrap";
-import { AuthProvider} from "@/app/firebase/AuthContext";
+// import Signup from "@/app/firebase/Signup";
+// import { Container } from "react-bootstrap";
+// import { AuthProvider} from "@/app/firebase/AuthContext";
+//
+// export default function App() {
+//   return (
+//       <AuthProvider>
+//     <Container
+//       className="d-flex align-items-center justify-content-center"
+//       style={{ minHeight: "100vh" }}
+//     >
+//       <div className="w-100" style={{ maxHeight: "400px" }}>
+//         <Signup />
+//       </div>
+//     </Container>
+//         </AuthProvider>
+//   );
+// }
 
-export default function App() {
+import SignIn from "@/app/firebase/SignIn";
+import SignUp from "@/app/firebase/SignUp";
+import AuthDetails from "@/app/firebase/AuthDetails";
+import "./firebase.css";
+
+function App() {
   return (
-      <AuthProvider>
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxHeight: "400px" }}>
-        <Signup />
-      </div>
-    </Container>
-        </AuthProvider>
+    <div className="App">
+      <section>
+        <SignIn />
+        <SignUp />
+        <AuthDetails />
+      </section>
+    </div>
   );
 }
+
+export default App;
