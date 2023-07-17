145ed092c7b5811c0c6a293f0777e16bc069ff97
jh0922 <vip0922@gmail.com>
2023-07-17 02:35:48 +0100

    -

--- frontend/src/app/firebase/page.js
+++ frontend/src/app/firebase/page.js
@@ -1,25 +1,6 @@
 "use client";
 import React from "react";
 
-// import Signup from "@/app/firebase/Signup";
-// import { Container } from "react-bootstrap";
-// import { AuthProvider} from "@/app/firebase/AuthContext";
-//
-// export default function App() {
-//   return (
-//       <AuthProvider>
-//     <Container
-//       className="d-flex align-items-center justify-content-center"
-//       style={{ minHeight: "100vh" }}
-//     >
-//       <div className="w-100" style={{ maxHeight: "400px" }}>
-//         <Signup />
-//       </div>
-//     </Container>
-//         </AuthProvider>
-//   );
-// }
-
 import SignIn from "@/app/firebase/SignIn";
 import SignUp from "@/app/firebase/SignUp";
 import AuthDetails from "@/app/firebase/AuthDetails";
@@ -38,3 +19,26 @@ function App() {
 }
 
 export default App;
+
+
+
+// old
+
+// import Signup from "@/app/firebase/Signup";
+// import { Container } from "react-bootstrap";
+// import { AuthProvider} from "@/app/firebase/AuthContext";
+//
+// export default function App() {
+//   return (
+//       <AuthProvider>
+//     <Container
+//       className="d-flex align-items-center justify-content-center"
+//       style={{ minHeight: "100vh" }}
+//     >
+//       <div className="w-100" style={{ maxHeight: "400px" }}>
+//         <Signup />
+//       </div>
+//     </Container>
+//         </AuthProvider>
+//   );
+// }
