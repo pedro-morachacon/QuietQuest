6d44fb1f5b29878fa72a3a0a5e2ce32b0ed1f1ef
AislingLC <aisling.cooke@ucdconnect.ie>
2023-07-18 19:24:44 +0100

    Added favicons for new logo

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -13,7 +13,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#000000" />
+
+        </head>
+      <body className={inter.className}>
+        {children}
+        </body>
     </html>
   )
 }
