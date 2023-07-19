7fdef3cafa475f6b137d0180c45449aa8c47be41
AislingLC <aisling.cooke@ucdconnect.ie>
2023-07-19 16:32:51 +0100

    Update page.js
    Added Tabs component, allowed props passing between Tabs and map with useState and useEffect

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,10 @@
 /* homepage */
 
 "use client"
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
+import Tabs from './components/Tabs'
+
 
 const MapWithNoSSR = dynamic(() => import('./components/DisplayMap'), {
   ssr: false,
@@ -10,14 +12,34 @@ const MapWithNoSSR = dynamic(() => import('./components/DisplayMap'), {
 
 
 export default function App() {
+
+
+  const [activeTab, setActiveTab] = useState(''); 
+  // Initialize the activeTab state
+
+
+  useEffect(() => {
+    // Code to run when the activeTab changes
+    // You can add any side effects or logic here
+    console.log('activeTab changed:', activeTab);
+  }, [activeTab]);
+  
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab); // Update the activeTab state when the tab is changed
+  };
+
   return (
       <main>
     <div className="App">
       {/*<div className="search-params-container">*/}
       {/*  <SearchParamsWithNoSSR />*/}
       {/*</div>*/}
+      <div>
+         <Tabs activeTab={activeTab} onTabChange={handleTabChange} />
+      </div>
       <div className="map-container">
-        <MapWithNoSSR />
+        <MapWithNoSSR activeTab={activeTab}/>
       </div>
       <style jsx>{`
         .App {
