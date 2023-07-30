553a62861a3963f65093d226fb12fbfebac67b67
MichaelMoyles <93867352+MichaelMoyles@users.noreply.github.com>
2023-07-30 19:13:57 +0100

    storing routes to firebase database

--- frontend/src/app/components/SavedRoutes.js
+++ frontend/src/app/components/SavedRoutes.js
@@ -4,13 +4,14 @@ import { doc, setDoc, getFirestore, addDoc, collection, getDocs, query, where, g
 import { db } from "@/app/firebase";
 
 const SavedRoutes = () => {
-  const [user, setUser] = useState(null);
   const [docData, setDocData] = useState(null);
 
-  const auth = getAuth();
-  setUser(auth.currentUser);
+
 
   useEffect(() => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
     // Using onAuthStateChanged for real-time listening
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
@@ -23,6 +24,7 @@ const SavedRoutes = () => {
             console.log('No such document!');
           } else {
             setDocData(doct.data());
+            console.log(docData);
           }
         } catch (error) {
           console.error("Error fetching documents: ", error);
@@ -41,25 +43,14 @@ const SavedRoutes = () => {
 
 };
 
-  const addRoute = async () => {
-    if (user) {
-      try {
-        const usersCollectionRef = collection(db, "users");
-        const userDocRef = doc(usersCollectionRef, user.uid);
-
-        const doct = await getDoc(userDocRef);
-        if (!doct.exists) {
-          console.log('No such document!');
-        } else {
-          setDocData(doct.data());
-        }
-      } catch (error) {
-        console.error("Error fetching documents: ", error);
-      }
-    } else {
-
-    }
-  };
+  // const addRoute = () => {
+  //       const path = "users/" + userUid;
+  //       // add data to Cloud Firestore
+  //       const docRef = addDoc(collection(db, path), {
+  //           Test: "test",
+  //       });
+  //       console.log("Document written with ID: ", docRef.id);
+  //   };
 
   const deleteRoute = () => {
 
@@ -72,11 +63,11 @@ const SavedRoutes = () => {
       </div>
       <div>
         <ul>
-          <ul>
-            <li>
-              <button onClick={addRoute}>Save Route</button>
-            </li>
-          </ul>
+          {/*<ul>*/}
+          {/*  <li>*/}
+          {/*    <button onClick={addRoute}>Save Route</button>*/}
+          {/*  </li>*/}
+          {/*</ul>*/}
           <ul>
             <li>Saved Route 1</li>
             <li>
