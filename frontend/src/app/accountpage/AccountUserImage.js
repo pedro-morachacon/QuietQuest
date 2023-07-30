76178c268a451ca9e3ead176d0e3d2da54cc3046
jh0922 <vip0922@gmail.com>
2023-07-30 18:22:50 +0100

    account page

---
+++ frontend/src/app/accountpage/AccountUserImage.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/app/firebase";
+
+function AccountUserImage() {
+  const [userDetails, setUserDetails] = useState(null);
+
+  useEffect(() => {
+    const authInstance = getAuth();
+
+    // Using onAuthStateChanged for real-time listening
+    const unsubscribe = onAuthStateChanged(authInstance, (user) => {
+      if (user) {
+        const details = {
+          displayName: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+          emailVerified: user.emailVerified,
+          uid: user.uid
+        };
+
+        setUserDetails(details);
+      } else {
+        setUserDetails(null);
+      }
+    });
+
+    // Unsubscribe from the listener when the component is unmounted
+    return () => {
+      unsubscribe();
+    };
+
+  }, []);
+
+  return (
+  <div>
+    {userDetails ? (
+      <div>
+        <a href="/accountpage">
+        {userDetails.photoURL && <img src={userDetails.photoURL} alt="User's profile" style={{ width: "100px", height: "100px", borderRadius: "50%" }} />}
+        </a>
+        {/*<p><strong>Display Name:</strong> {userDetails.displayName}</p>*/}
+        {/*<p><strong>Email:</strong> {userDetails.email}</p>*/}
+        {/*<p><strong>Email Verified:</strong> {String(userDetails.emailVerified)}</p>*/}
+        {/*<p><strong>UID:</strong> {userDetails.uid}</p>*/}
+      </div>
+    ) : (
+      <p>No user is currently signed in.</p>
+    )}
+  </div>
+);
+}
+
+export default AccountUserImage;
+
