1b84fd08e77f40bc09b8463363951d0655b5f91a
AislingLC <aisling.cooke@ucdconnect.ie>
2023-08-03 18:00:37 +0100

    Icons are in
    Alignment and mobile responsiveness to be fixed

--- frontend/src/app/components/Tabs.js
+++ frontend/src/app/components/Tabs.js
@@ -7,10 +7,10 @@ const Tabs = ({ activeTab, onTabChange }) => {
     <div className='Tabs'>
         {/* Tab navigation */}
         <ul className="nav">
-            <TabNavItem title="Map" id="map-only" activeTab={activeTab} onTabChange={onTabChange} imageSrc="./map1.svg"/>
+            <TabNavItem title="Map" id="map-only" activeTab={activeTab} onTabChange={onTabChange} imageSrc="./map1.svg" />
             <TabNavItem title="Noise" id="noise" activeTab={activeTab} onTabChange={onTabChange} imageSrc="./noise1.svg"/>
-            <TabNavItem title="Crowds" id="crowds" activeTab={activeTab} onTabChange={onTabChange} imageSrc="./crowds1.svvg"/>
-            <TabNavItem title="Both" id="both" activeTab={activeTab} onTabChange={onTabChange} imageSrc="./both1.svg"/>
+            <TabNavItem title="Crowds" id="crowds" activeTab={activeTab} onTabChange={onTabChange} imageSrc="./people2.svg"/>
+            <TabNavItem title="Both" id="both" activeTab={activeTab} onTabChange={onTabChange} imageSrc="./both7.svg"/>
         </ul>
         <div className="outlet">
            
