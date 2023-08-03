afb681d3b7f73798eab6e19f35c4b5506bf2da5e
MichaelMoyles <93867352+MichaelMoyles@users.noreply.github.com>
2023-08-03 12:09:08 +0100

    Improved display of autocomplete boxes

--- frontend/src/app/components/StartSearchField.js
+++ frontend/src/app/components/StartSearchField.js
@@ -84,20 +84,20 @@ const StartSearchField = ({ setStartLocation, currentLocation }) => {
 
 
   return (
-    <form id="start-search-form">
+    <form id="start-search-form" style={{ margin: '0 0 10px 2px' }}>
       <input
           className={"autoCompleteResults"}
           ref={inputRef} // Use the inputRef here
           type="text"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
-          placeholder="Enter address"
-          style={{ marginLeft: '1px' }}
+          placeholder="Starting Location"
+          style={{ borderRadius: '5px', padding:'5px' }}
       />
       {autocompleteResults.length > 0 && (
-        <ul style={{ position: 'absolute', zIndex: '99', marginTop:'2px' }}>
+        <ul style={{ position: 'absolute', zIndex: '99' }}>
           {autocompleteResults.map((result, index) => (
-            <li key={index} onClick={() => handleListItemClick(result)} style={{ backgroundColor: 'rgba(57,75,86,255)', color: 'white', border: '1px solid white', borderRadius: '5px', padding: '2px 2px 2px 4px'}} >
+            <li key={index} onClick={() => handleListItemClick(result)} style={{ backgroundColor: 'rgba(57,75,86,255)', color: 'white', border: '1px solid white', borderRadius: '5px', padding: '2px 2px 2px 4px', margin:'1px'}} >
               {result.label.split(', New York County')[0]}
             </li>
           ))}
